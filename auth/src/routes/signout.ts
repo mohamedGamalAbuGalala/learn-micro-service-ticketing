d26c32f6bdbeea773e69131fcbb441678554d446
Mohamed Gamal <mohamed.abugalala@gmail.com>
2022-12-08 20:33:29 +0200

    Finish signout api

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,11 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
-router.post(
-  "/api/users/signout",
-  (req, res) => {
-    return res.send("Hi there!!");
-  }
-);
+router.post("/api/users/signout", (req, res) => {
+  req.session = null;
+
+  return res.send({});
+});
 
 export { router as signoutRouter };
