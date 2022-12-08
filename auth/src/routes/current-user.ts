af033ea3e310ef7ec88427ae56c463f37aca5571
Mohamed Gamal <mohamed.abugalala@gmail.com>
2022-12-08 20:29:13 +0200

    Finish current user url

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,25 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get(
-  "/api/users/currentuser",
+  "/api/users/currentUser",
   (req, res) => {
-    return res.send("Hi there!!");
+    if (!req.session?.jwt) {
+      return res.send({ currentUser: null });
+    }
+
+    try {
+      const payload = jwt.verify(
+        req.session.jwt,
+        process.env.JWT_KEY!
+      );
+
+      return res.send({ currentUser: payload });
+    } catch (error) {
+      return res.send({ currentUser: null });
+    }
   }
 );
 
