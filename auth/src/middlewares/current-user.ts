dbfd5d22b0a94615de3cc027768db15971915230
Mohamed Gamal <mohamed.abugalala@gmail.com>
2022-12-08 21:34:21 +0200

    Finish current-user middleware

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+export const currentUser = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+
+    console.log("User is signed in!");
+    req.currentUser = payload;
+  } catch (error) {}
+
+  return next();
+};
